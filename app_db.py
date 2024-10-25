7b30256c4f6b8cb88faec96e3f31675ae9a9a1be
Emythiel <18901520+Emythiel@users.noreply.github.com>
2024-10-25 10:10:59 +0200

    add rooms csv and make populate db with data when created

--- app_db.py
+++ app_db.py
@@ -1,4 +1,4 @@
-import sqlite3, os
+import sqlite3, os, csv
 
 db_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'app-db', 'rooms.db')
 os.makedirs(os.path.dirname(db_path), exist_ok=True)
@@ -7,11 +7,21 @@ os.makedirs(os.path.dirname(db_path), exist_ok=True)
 def db_create():
     with sqlite3.connect(db_path) as conn:
         cur = conn.cursor()
+        # Create table if it doesn't exist
         cur.execute("""CREATE TABLE IF NOT EXISTS rooms (
                     roomId INTEGER PRIMARY KEY,
                     type TEXT
                     )""")
-    print(f'Database created at {db_path}')
+
+        # Read and insert data from CSV file
+        with open('roomsDatabase.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader) # Skip header row if present
+            rows = [(int(row[0]), row[1]) for row in reader]
+            # Insert rows into the database table
+            cur.executemany('INSERT INTO rooms (roomId, type) VALUES (?, ?)', rows)
+
+    print(f'Database created and populated at {db_path}')
 
 # Get list of all rooms
 def get_all():
